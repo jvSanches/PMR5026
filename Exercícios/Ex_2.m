34b932cf36af614f17f26b5b99b72e6e6b54795a
Victor Chacon <victor.codesseira@gmail.com>
2020-03-02 01:16:31 -0300

    Finish ex2

--- Exercícios/Ex_2.m
+++ Exercícios/Ex_2.m
@@ -1,40 +1,72 @@
 clear all
 close all
-syms x x_1 x_2 rho g A L
+%% Cálculo da matriz de rigidez
+syms E A L
 
+x_i = linspace(0,L,5);
+xm_i = [x_i(2) + x_i(1), x_i(3) + x_i(2), x_i(4) + x_i(3), x_i(5) + x_i(4)]/2;
+L_i = [L/4, L/4, L/4, L/4];
+A_i = 2*A - xm_i*(A/L); 
+
+K1 = (E*A_i(1)/(L_i(1)))*[1, -1; -1, 1];
+K2 = (E*A_i(2)/(L_i(2)))*[1, -1; -1, 1];
+K3 = (E*A_i(3)/(L_i(3)))*[1, -1; -1, 1];
+K4 = (E*A_i(4)/(L_i(4)))*[1, -1; -1, 1];
+
+K = [K1(1,1),           K1(1,2),                 0,                 0,       0;
+     K1(2,1), K1(2,2) + K2(1,1),           K2(1,2),                 0,       0;
+           0,           K2(2,1), K2(2,2) + K3(1,1),           K3(1,2),       0;
+           0,                 0,           K3(2,1), K3(2,2) + K4(1,1), K4(1,2);
+           0,                 0,                 0,           K4(2,1), K4(2,2)];
+
+%% Cálculo do vetor de forças distribuídas
+syms x_1 x_2 x rho g A R P
+
+% Usando as funções de forma
 N1 = (x_2-x)/(L/4);
 N2 = (x-x_1)/(L/4);
 F = rho*g*A*(2-x/L);
 F_1 = int(F*N1,x_1,x_2);
 F_2 = int(F*N2,x_1,x_2);
 
-F = [F_1; F_2];
+F = [F_1; F_2]; % Matriz de forças elementar
 
-L_ = 300;
-A_ = 80;
-P_ = 45;
-E_ = 72;
-g_ = 9.8;
-rho_ = 2.7000E-06;
+% Substituíndo para cada nó
+F1 = subs(F,[x_1,x_2],[x_i(1), x_i(2)]);
+F2 = subs(F,[x_1,x_2],[x_i(2), x_i(3)]);
+F3 = subs(F,[x_1,x_2],[x_i(3), x_i(4)]);
+F4 = subs(F,[x_1,x_2],[x_i(4), x_i(5)]);
 
-x_ = linspace(0,L_,5);
-F1 = subs(F,[A,L,rho,g,x_1,x_2],[A_,L_,rho_,g_,x_(1), x_(2)]);
-F2 = subs(F,[A,L,rho,g,x_1,x_2],[A_,L_,rho_,g_,x_(2), x_(3)]);
-F3 = subs(F,[A,L,rho,g,x_1,x_2],[A_,L_,rho_,g_,x_(3), x_(4)]);
-F4 = subs(F,[A,L,rho,g,x_1,x_2],[A_,L_,rho_,g_,x_(4), x_(5)]);
+% R é a força de reação, P é a força externa
+Fg = [F1(1) + R; F1(2) + F2(1); F2(2) + F3(1); F3(2) + F4(1); F4(2) + P];
 
-syms R u2 u3 u4 u5
-K = (E_*A_/L_)*[15/2 -15/2 0 0 0; -15/2 14 -13/2 0 0; 0 -13/2 12 -11/2 0; 0 0 -11/2 10 -9/2; 0 0 0 -9/2 9/2];
-F = [F1(1) + R; F1(2) + F2(1); F2(2) + F3(1); F3(2) + F4(1); F4(2) + P_];
+%% Cálculos numéricos
+L = 300;
+A = 80;
+P = 45;
+E = 72;
+g = 9.8;
+rho = 0; % rho = 0, pois desprezamos o peso próprio
 
+% Substituindo valores
+x_i = eval(x_i);
+K = eval(K);
+Fg = eval(Fg);
+
+% Removendo o nó engastado
 K(1,:) = 0;
 K(:,1) = 0;
 K(1,1) = 1;
-F(1) = 0;
+Fg(1) = 0;
 
-u = double(linsolve(K,F));
+% Resolvendo o sistema linear
+u = double(linsolve(K,Fg));
 
-fplot(@(x) ((P_*L_)/(E_*A_))*log((2*L_)/(2*L_ - x)));
+plot(x_i,u,'*-');
 hold on
-plot(x_,u,'*');
+fplot(@(x) ((P*L)/(E*A))*log((2*L)/(2*L - x)), [0 300]);
 
+legend( "Solução aproximada","Solução analítica")
+legend('Location', 'NW')
+xlabel("Posição do nó")
+ylabel("Deslocamentos")
